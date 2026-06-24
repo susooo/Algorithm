c3c23cf2d12fe97c2d4313aa83e5564855516171
김수현 <92291198+susooo@users.noreply.github.com>
2026-06-24 23:35:17 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 228.41 ms, Memory: 15.2 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,55 +1,45 @@
 from collections import deque
 
 def solution(land):
-    n = len(land)
-    m = len(land[0])
     
-    visited = [[False]*m for _ in range(n)]
+    n,m = len(land), len(land[0])
     dxy = [(0,1),(1,0),(0,-1),(-1,0)]
     
-    #땅 위치 판별
+    board = [[-1]*m for _ in range(n)]
+    oil_cnt = dict()
+    
     def in_range(x,y):
         return 0<=x<n and 0<=y<m
     
-    #석유 덩어리 추출
-    oil_grid = [[-1]*m for _ in range(n)]
-    oil_sizes = dict()
-    oil_num = 0
+    def bfs(i,j,cnt):
+        nonlocal num
+        dq = deque([(i,j)])
+        
+        while dq:
+            x,y = dq.popleft()
+            
+            for dx,dy in dxy:
+                nx,ny = x+dx, y+dy
+                
+                if in_range(nx,ny) and land[nx][ny] == 1 and board[nx][ny] == -1:
+                    board[nx][ny] = num
+                    cnt += 1
+                    dq.append((nx,ny))
+                    
+        oil_cnt[num] = cnt
+        
+    num = 1
     for i in range(n):
         for j in range(m):
-            if not land[i][j] or visited[i][j]:
+            if land[i][j]==0 or board[i][j] != -1:
                 continue
+            board[i][j] = num
+            bfs(i,j,1)
+            num += 1
             
-            q = deque()
-            q.append((i,j))
-            visited[i][j] = True
-            oil_grid[i][j] = oil_num
-            size = 1
-            
-            while q:
-                x,y = q.popleft()
-                for dx,dy in dxy:
-                    nx,ny = x+dx, y+dy
-                    if in_range(nx,ny) and land[nx][ny] and not visited[nx][ny]:
-                        visited[nx][ny] = True
-                        q.append((nx,ny))
-                        size += 1
-                        oil_grid[nx][ny] = oil_num
-            
-            oil_sizes[oil_num] = size
-            oil_num += 1
-            
-    #열마다 포함된 덩어리
-    answer = 0
-    for col in range(m):
-        check = set()
-        curr = 0
-        for row in range(n):
-            num = oil_grid[row][col]
-            if num != -1 and num not in check:
-                check.add(num)
-                curr += oil_sizes[num]
-                
-        answer = max(answer, curr)
-    
+    answer = 0   
+    for j in range(m):
+        oil_group = {board[i][j] for i in range(n) if board[i][j] != -1}
+        answer = max(answer, sum(oil_cnt[k] for k in oil_group))
+        
     return answer
