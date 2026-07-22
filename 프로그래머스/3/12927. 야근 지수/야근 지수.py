9e35170fa58f5c0d0b0c98dd96a5153f43473609
김수현 <92291198+susooo@users.noreply.github.com>
2026-07-22 15:00:30 +0900

    [level 3] Title: 야근 지수, Time: 326.23 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,41 +1,21 @@
+import heapq
+
 def solution(n, works):
-    
+
     if sum(works) <= n:
         return 0
-    
-    #평균값 찾기
-    left,right = 0, max(works)
-    while left < right:
-        mid = (left+right)//2
-        
-        need = 0
-        for w in works:
-            if w > mid:
-                need += w-mid
-            
-        if need > n:
-            left = mid + 1
-        else:
-            right = mid
-            
-    level = left
-    remain = n
-    
-    #평균값까지 작업량 줄이기
-    works.sort(reverse=True)
-    for i in range(len(works)):
-        if works[i] > level:
-            remain -= works[i]-level
-            works[i] = level
-        else:
-            break
-            
-    #남은 시간 재분배
-    for i in range(len(works)):
-        if remain == 0:
-            break
-            
-        works[i] -= 1
-        remain -= 1
-        
-    return sum(w**2 for w in works)
+
+    # 최대 힙
+    heap = [-w for w in works]
+    heapq.heapify(heap)
+
+    while n:
+        x = -heapq.heappop(heap)   # 가장 큰 작업
+
+        x -= 1                     # 1시간 작업
+
+        heapq.heappush(heap, -x)
+
+        n -= 1
+
+    return sum(x * x for x in map(lambda x: -x, heap))
