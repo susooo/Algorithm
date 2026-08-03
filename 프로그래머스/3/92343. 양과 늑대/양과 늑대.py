6bbc6b4187238ca12ab0f267ba1332f6d41e379b
김수현 <92291198+susooo@users.noreply.github.com>
2026-08-03 11:24:56 +0900

    [level 3] Title: 양과 늑대, Time: 21.40 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,26 +1,39 @@
 def solution(info, edges):
     answer = 0
+    n = len(info)
     
-    graph = [[] for _ in range(len(info))]
+    graph = [[] for _ in range(n)]
     for a,b in edges:
         graph[a].append(b)
-        graph[b].append(a)
         
-    def dfs(available, sheep, wolf):
+    def dfs(mask, candidates):
         nonlocal answer
+        sheep, wolf = 0,0
+        
+        for i in range(n):
+            if mask & (1<<i):
+                if info[i] == 0:
+                    sheep+=1
+                else:
+                    wolf+=1
         
         if wolf >= sheep:
             return
         
         answer = max(answer, sheep)
+        
+        for node in candidates:
+            new_mask = mask | (1<<node)
+            
+            new_candidates = candidates.copy()
+            new_candidates.remove(node)
+            
+            for child in graph[node]:
+                if not (new_mask & (1 << child)):
+                    new_candidates.add(child)
 
-        for node in list(available):
-            for nx in graph[node]:
-                if nx not in available:
-                    if info[nx]==0:
-                        dfs(available|{nx}, sheep+1, wolf)
-                    else:
-                        dfs(available|{nx}, sheep, wolf+1)
-                    
-    dfs(set([(0)]), 1, 0)
+            dfs(new_mask, new_candidates)
+
+    dfs(1<<0, set(graph[0]))
+    
     return answer
